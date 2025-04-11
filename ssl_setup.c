49578e1997bbcfb32c6d1a4439f2cab1af1b130e
0T0M8 <lawrencelipenga2020@gmail.com>
2025-04-11 18:31:40 +0200

    Create ssl_setup.c

---
+++ ssl_setup.c
@@ -0,0 +1,27 @@
+#include "server.h"
+
+void init_openssl() {
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+}
+
+SSL_CTX *create_ssl_context() {
+    SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_certificate_file(ctx, "certs/server.crt", SSL_FILETYPE_PEM) <= 0 ||
+        SSL_CTX_use_PrivateKey_file(ctx, "certs/server.key", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void cleanup_openssl() {
+    EVP_cleanup();
+}
